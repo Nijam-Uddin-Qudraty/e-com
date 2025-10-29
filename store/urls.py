abe12fb2ef8285872100414b9c73e6d98afa5b97
Nijam-Uddin-Qudraty <129588187+Nijam-Uddin-Qudraty@users.noreply.github.com>
2025-10-29 07:54:56 +0600

    add context provider
    updated category in both nav and store

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.StoreView,name='store')
+    path("",views.StoreView,name='store'),
+    path("<slug:category_slug>/",views.StoreView,name='products_by_category'),
 ] 
--- store/views.py
+++ store/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Product
+from category.models import Category
 
-def StoreView(req):
-    products = Product.objects.all().filter(is_available=True)
-    product_count = products.count()
+def StoreView(req,category_slug=None):
+    category = None
+    products = None
+    product_count = 0
+    if category_slug != None:
+        category = get_object_or_404(Category,slug = category_slug)
+        products = Product.objects.filter(category = category)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count = products.count()
+
     context= {
         'products':products,
         'product_count': product_count
