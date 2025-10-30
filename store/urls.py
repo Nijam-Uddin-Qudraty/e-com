5bcba5669337654a5da00fadb61352b1d71ae00b
Nijam-Uddin-Qudraty <129588187+Nijam-Uddin-Qudraty@users.noreply.github.com>
2025-10-30 08:11:41 +0600

    update detail view
    changed cover but isnt showing

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("",views.StoreView,name='store'),
     path("<slug:category_slug>/",views.StoreView,name='products_by_category'),
+    path("<slug:category_slug>/<slug:product_slug>/",views.product_details,name='product_detail'),
 ] 
